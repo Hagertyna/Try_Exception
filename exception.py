57db9549d8ee0fda97e8ec30a9d35b7aabc5e03b
Hagertyna <100336341+Hagertyna@users.noreply.github.com>
2022-03-08 05:52:04 -0800

    Add files via upload
    Exceptions in python

---
+++ exception.py
@@ -0,0 +1,22 @@
+try:
+    """
+    The code which can give rise to an exception is written here
+    """
+    a="10"
+    b= int(a)
+    print("I am executing")
+    print("b =",b)
+except:
+    print("Exception caught")
+
+# now lets caught exception
+try:
+    """
+    The code which can give rise to an exception is written here
+    """
+    a="hagi"
+    b= int(a)
+    print("I am executing")
+    print("b =",b)
+except:
+    print("Exception caught")
---
+++ exceptionraise.py
@@ -0,0 +1,20 @@
+#catching specific exception
+from tkinter import Y
+
+
+try:
+    a = int(input("Please enter the first number..."))
+    b = int(input("The second number please"))
+    if(a <0):
+        raise TypeError
+    c= x/y
+    print("{} /{} = {}".format(a,b,c))
+except ZeroDivisionError:
+    print("Division by zero is impossible")
+except ValueError:
+    print("the data types are not proper")
+except TypeError:
+    print("Data types are not in range")
+except NameError:
+    print("Data items are not defined")
+   
